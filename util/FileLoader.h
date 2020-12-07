fe53cbd028341f51c368f07f209dc31f2b6742a3
Moose <51682381+moosezz@users.noreply.github.com>
2020-12-06 20:07:34 -0500

    Create FileLoader.h

---
+++ util/FileLoader.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+typedef std::string string;
+
+class FileLoader
+{
+public:
+	static bool LoadAsStringVector(char* file, std::vector<string>* content)
+	{
+		std::ifstream input(file); //Open file specified in args
+
+		if (input.is_open() == false) //Make sure we've opened it successfully
+		{
+			std::cout << "Failed to read file\n";
+			return false;
+		}
+		string str;
+
+
+		//Read the input file, build the array
+		while (std::getline(input, str))
+		{
+			content->emplace_back(str);
+		}
+
+		input.close();
+		return true;
+	}
+};
