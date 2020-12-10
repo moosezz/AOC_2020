d56d24e991314c19043eb2c60960ab01a998540b
Moose <51682381+purin-dev@users.noreply.github.com>
2020-12-10 16:51:49 -0500

    Update Day9.cpp

--- Day9/Day9.cpp
+++ Day9/Day9.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
 	std::vector<long long int> input;
 	if (FileLoader::LoadAsLongLongIntVector(argv[1], &input) == false) return 0;
 
-	
+
 	int idx = 0;
 	bool running = true;
 
@@ -23,34 +23,33 @@ int main(int argc, char* argv[])
 	idx += 25;
 
 	long long int outlier;
-	while(running)
+	while (running)
 	{
 		bool outlier_found = true;
 		std::vector<long long int> buffer;
-		for (int i = 1; i < iv+1; i++)
+		for (int i = 1; i < iv + 1; i++)
 		{
 			buffer.emplace_back(input[idx - i]);
 		}
 
 		for (auto it1 = buffer.begin(); it1 != buffer.end(); ++it1)
 		{
-			for(auto it2 = buffer.begin(); it2 != buffer.end(); ++it2)
+			for (auto it2 = buffer.begin(); it2 != buffer.end(); ++it2)
 			{
-				if(it1 != it2 && *it1+*it2 == input[idx])
+				if (it1 != it2 && *it1 + *it2 == input[idx])
 				{
 					outlier_found = false;
 					break;
-					
 				}
 			}
 		}
-		if(outlier_found)
+		if (outlier_found)
 		{
-		std::cout << "found outlier: " << input[idx] << "\n";
-		outlier = input[idx];
-		running = false;
+			std::cout << "found outlier: " << input[idx] << "\n";
+			outlier = input[idx];
+			running = false;
 		}
-		
+
 		idx++;
 	}
 
@@ -60,14 +59,13 @@ int main(int argc, char* argv[])
 		buffer.emplace_back(*it);
 
 		int i = 1;
-		
-		while (std::accumulate(buffer.begin(),buffer.end(),0) < outlier)
+
+		while (std::accumulate(buffer.begin(), buffer.end(), 0) < outlier)
 		{
-			buffer.emplace_back(*(it+i));
+			buffer.emplace_back(*(it + i));
 			i++;
-			
 		}
-		if(std::accumulate(buffer.begin(),buffer.end(),0) == outlier)
+		if (std::accumulate(buffer.begin(), buffer.end(), 0) == outlier)
 		{
 			std::cout << "found string of numbers: ";
 			for (auto value : buffer)
@@ -75,13 +73,10 @@ int main(int argc, char* argv[])
 				std::cout << value << " ";
 			}
 			std::cout << "\n";
-			auto min = std::min_element(buffer.begin(),buffer.end());
-			auto max = std::max_element(buffer.begin(),buffer.end());
-			std::cout << "min: " << *min << "\n" << "max: " << *max << "\n" << "sum: " << *min+*max << "\n";
+			auto min = std::min_element(buffer.begin(), buffer.end());
+			auto max = std::max_element(buffer.begin(), buffer.end());
+			std::cout << "min: " << *min << "\n" << "max: " << *max << "\n" << "sum: " << *min + *max << "\n";
 			break;
 		}
-		
 	}
-
-	
 }
