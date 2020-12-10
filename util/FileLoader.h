df1f421c93c0b1246a4317835b4cc06824cef36e
Moose <51682381+purin-dev@users.noreply.github.com>
2020-12-10 16:52:26 -0500

    Update FileLoader.h

--- util/FileLoader.h
+++ util/FileLoader.h
@@ -30,4 +30,25 @@ public:
 		input.close();
 		return true;
 	}
+	static bool LoadAsLongLongIntVector(char* file, std::vector<long long int>* content)
+	{
+		std::ifstream input(file); //Open file specified in args
+
+		if (input.is_open() == false) //Make sure we've opened it successfully
+		{
+			std::cout << "Failed to read file\n";
+			return false;
+		}
+		string str;
+
+
+		//Read the input file, build the array
+		while (std::getline(input, str))
+		{
+			content->emplace_back(std::stoll(str));
+		}
+
+		input.close();
+		return true;
+	}
 };
